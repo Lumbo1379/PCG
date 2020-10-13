ca8c4872e6b51f7ddd67f21a6799f270a30519fc
Lumbo1379 <36245864+Lumbo1379@users.noreply.github.com>
2020-10-13 20:44:13 +0100

    Unit test run

--- Assets/Editor/JenkinsBuild.cs
+++ Assets/Editor/JenkinsBuild.cs
@@ -3,19 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
+using UnityEditor.TestTools.TestRunner.Api;
 using UnityEngine;
 
 class JenkinsBuild
 {
-    static string[] SCENES = FindEnabledEditorScenes();
+    private static string[] SCENES = FindEnabledEditorScenes();
 
-    static string APP_NAME = "PCG";
-    static string TARGET_DIR = "target";
+    private static string APP_NAME = "PCG";
+    private static string TARGET_DIR = "target";
 
-    static void BuildWindows64()
+    private static void BuildWindows64()
     {
         string targetDir = APP_NAME + ".app";
         GenericBuild(SCENES, TARGET_DIR + "/" + targetDir, BuildTarget.StandaloneWindows64, BuildOptions.None);
+    }
+
+    private static void RunEditorUnitTest()
+    {
+        var testRunnerApi = ScriptableObject.CreateInstance<TestRunnerApi>();
+        var filter = new Filter()
+        {
+            testMode = TestMode.EditMode
+        };
+        testRunnerApi.Execute(new ExecutionSettings(filter));
+
     }
 
     private static string[] FindEnabledEditorScenes()
@@ -23,7 +35,7 @@ class JenkinsBuild
         return (from scene in EditorBuildSettings.scenes where scene.enabled select scene.path).ToArray();
     }
 
-    static void GenericBuild(string[] scenes, string targetDir, BuildTarget buildTarget, BuildOptions buildOptions)
+    private static void GenericBuild(string[] scenes, string targetDir, BuildTarget buildTarget, BuildOptions buildOptions)
     {
         EditorUserBuildSettings.SwitchActiveBuildTarget(buildTarget);
         BuildPipeline.BuildPlayer(scenes, targetDir, buildTarget, buildOptions).ToString();
