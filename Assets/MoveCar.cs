837476c42883295b3550ba6917773b24152ad6bc
Callum Thompson <36245864+Lumbo1379@users.noreply.github.com>
2021-01-24 03:15:38 +0000

    Car movement with markers

--- Assets/MoveCar.cs
+++ Assets/MoveCar.cs
@@ -5,14 +5,41 @@ using UnityEngine;
 public class MoveCar : MonoBehaviour
 {
     [SerializeField] private bool _useKeyboardInput;
+    [SerializeField] private bool _useTestMarkers;
     [SerializeField] private WheelCollider[] _wheelsColliders;
     [SerializeField] private Transform[] _wheelsTransforms;
     [SerializeField] [Range(0, 100)] private float _maxSteerAngle = 30;
     [SerializeField] [Range(0, 500)] private float _torque = 50;
+    [SerializeField] [Range(0, 5)] private float _torqueToSteerRatio = 2;
+    [SerializeField] [Range(0, 5)] private float _minNextMarkerDistance = 1;
+    [SerializeField] private Transform[] _testMarkers;
 
     private float _horizontalInput;
     private float _verticalInput;
     private float _steeringAngle;
+    private int _markerIndex;
+    private Transform _target;
+    private float _torqueWeight;
+
+    private void Awake()
+    {
+        _markerIndex = 0;
+
+        if (_useTestMarkers)
+            GetNextMarkerTransform();
+    }
+
+    private void Update()
+    {
+        if (_useTestMarkers)
+        {
+            if (_target == null)
+                GetNextMarkerTransform();
+
+            if (Vector3.Distance(transform.position, _target.position) <= _minNextMarkerDistance)
+                GetNextMarkerTransform();
+        }
+    }
 
     private void FixedUpdate()
     {
@@ -30,9 +57,22 @@ public class MoveCar : MonoBehaviour
         _verticalInput = Input.GetAxis("Vertical");
     }
 
+    private void GetNextMarkerTransform()
+    {
+        if (_markerIndex == _testMarkers.Length)
+            _markerIndex = 0;
+
+        _target = _testMarkers[_markerIndex];
+
+        _markerIndex++;
+    }
+
     private void Steer()
     {
-        _steeringAngle = _maxSteerAngle * _horizontalInput;
+        if (_useKeyboardInput)
+            _steeringAngle = _maxSteerAngle * _horizontalInput;
+        else if (_useTestMarkers)
+            _steeringAngle = _maxSteerAngle * GetSteeringAngle();
 
         for (int i = 0; i < 2; i++)
             _wheelsColliders[i].steerAngle = _steeringAngle;
@@ -41,7 +81,12 @@ public class MoveCar : MonoBehaviour
     private void Accelerate()
     {
         for (int i = 0; i < 2; i++)
-            _wheelsColliders[i].motorTorque = _torque * _verticalInput;
+        {
+            if (_useKeyboardInput)
+                _wheelsColliders[i].motorTorque = _torque * _verticalInput;
+            else if (_useTestMarkers)
+                _wheelsColliders[i].motorTorque = _torque * (1 - _torqueWeight / _torqueToSteerRatio);
+        }
     }
 
     private void UpdateWheelPositionAndRotation()
@@ -56,5 +101,33 @@ public class MoveCar : MonoBehaviour
             _wheelsTransforms[i].position = position;
             _wheelsTransforms[i].rotation = rotation;
         }
+    }
+
+    private float GetSteeringAngle()
+    {
+        if (_target == null) return 0;
+
+        var direction = _target.position - transform.position;
+        float angle = Vector3.Angle(direction, transform.forward);
+        angle = Mathf.Clamp(angle / _maxSteerAngle, 0, 1);
+
+        _torqueWeight = angle;
+
+        bool isLeft = IsLeft(direction);
+
+        if (isLeft) angle *= -1;
+
+        return angle;
+    }
+
+    private bool IsLeft(Vector3 targetDirection)
+    {
+        var perpendicular = Vector3.Cross(transform.forward, targetDirection);
+        var direction = Vector3.Dot(perpendicular, transform.up);
+
+        if (direction < 0)
+            return true;
+
+        return false;
     }
 }
